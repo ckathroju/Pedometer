082e2573798910accfba2ae9d15b9067779f3444
Srinath Natarajan <sriyegna@gmail.com>
2021-03-10 11:55:43 -0500

    Add table of data to weight tracker and ui changes

--- screens/loggedIn/tabs/TabScreen2.js
+++ screens/loggedIn/tabs/TabScreen2.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { useTheme } from "@react-navigation/native";
-import { dateToEpoch } from "../../../utils/datetime";
+import { getCurrentDateInEpoch, epochToDate } from "../../../utils/datetime";
 import * as SQLite from "expo-sqlite";
-import { TextInput, Button } from "react-native-paper";
+import { TextInput, Button, DataTable } from "react-native-paper";
 //imported this from constants
 import { DB_FILE, DB_WEIGHT_TABLE } from "../../../constants";
 
@@ -15,15 +15,23 @@ export const TabScreen2 = ({ navigation }) => {
   const { colors } = useTheme();
   const theme = useTheme();
   const [data, setData] = useState(null);
+  const [tableData, setTableData] = useState([]);
 
-  //look at line 23
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    db.transaction((tx) => {
+      tx.executeSql(`SELECT * FROM ${DB_WEIGHT_TABLE}`, [], (_, { rows }) => {
+        console.log(JSON.stringify(rows["_array"]));
+        setTableData(rows["_array"]);
+      });
+    });
+  };
+
   const saveData = () => {
-    const dt = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate()
-    );
-    const id = dateToEpoch(dt);
+    const id = getCurrentDateInEpoch();
     db.transaction((tx) => {
       tx.executeSql(
         `INSERT INTO ${DB_WEIGHT_TABLE} (id, weight) VALUES (${id}, ${data})`,
@@ -35,16 +43,39 @@ export const TabScreen2 = ({ navigation }) => {
 
   return (
     <View>
-      <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
-      <Text style={{ color: colors.text }}>Enter your weight today.</Text>
-      <TextInput
-        label="Weight"
-        value={data}
-        onChangeText={(text) => setData(text)}
-      />
-      <Button mode="contained" onPress={() => saveData()}>
-        Save Weight
-      </Button>
+      <View>
+        <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
+        <Text style={{ color: colors.text }}>Enter your weight today.</Text>
+        <TextInput
+          label="Weight"
+          value={data}
+          onChangeText={(text) => setData(text)}
+          keyboardType="decimal-pad"
+        />
+        <Button mode="contained" onPress={() => saveData()}>
+          Save Weight
+        </Button>
+      </View>
+      <View>
+        <View>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Date</DataTable.Title>
+              <DataTable.Title>Weight</DataTable.Title>
+            </DataTable.Header>
+            {tableData.map((x) => {
+              return (
+                <DataTable.Row key={x.id}>
+                  <DataTable.Cell>
+                    {epochToDate(x.id).toDateString()}
+                  </DataTable.Cell>
+                  <DataTable.Cell>{x.weight}</DataTable.Cell>
+                </DataTable.Row>
+              );
+            })}
+          </DataTable>
+        </View>
+      </View>
     </View>
   );
 };
@@ -67,7 +98,7 @@ const TabStackScreen2 = ({ navigation }) => {
         name="Tab2"
         component={TabScreen2}
         options={{
-          title: "Tab2",
+          title: "Weight",
           headerLeft: () => (
             <Icon.Button
               name="menu"

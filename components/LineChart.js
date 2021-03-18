63bd43c2c8c3a126a7d12bbc09acf689eada8057
Srinath Natarajan <sriyegna@gmail.com>
2021-03-18 13:23:19 -0400

    VictoryChart + update db name

--- components/LineChart.js
+++ components/LineChart.js
@@ -1,109 +1,34 @@
-import React from 'react';
+import React from "react";
 import { View } from "react-native";
 // import ReactDOM from 'react-dom';
 // import * as V from 'victory';
-import { VictoryLine, VictoryChart, VictoryAxis,VictoryTheme } from 'victory-native';
-import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  Text,
-  TSpan,
-  TextPath,
-  Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
-} from 'react-native-svg';
-
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryTheme,
+} from "victory-native";
 
 function LineChart() {
-  // const theme={VictoryTheme.material};
-
-  // const style={{
-  //   data: { stroke: "#c43a31" },
-  //   parent: { border: "1px solid #ccc"}
-  // }};
-
-  const data = [
-    {quarter: 1, earnings: 13000},
-    {quarter: 2, earnings: 16500},
-    {quarter: 3, earnings: 14250},
-    {quarter: 4, earnings: 19000}
-  ];
 
   return (
     <View>
-      <VictoryChart
-        theme={VictoryTheme.material}
-      >
+      <VictoryChart theme={VictoryTheme.material}>
         <VictoryLine
           style={{
             data: { stroke: "#c43a31" },
-            parent: { border: "1px solid #ccc"}
+            parent: { border: "1px solid #ccc" },
           }}
-          data={data}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 },
+          ]}
         />
       </VictoryChart>
     </View>
   );
-};
+}
 
-// import { Line } from 'react-chartjs-2'
-// function LineChart() {
-
-//     const data = { 
-//         labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
-//         datasets: [ 
-//             {
-//                 label: 'Daily Step Count',
-//                 data: [100, 200, 300, 223, 200, 300, 200],
-//                 fill: true,
-//                 backgroundColor: "rgba(75,192,192,0.2)",
-//                 borderColor: "rgba(75,192,192,1)"
-//             }
-//         ]
-//     };
-
-//     const legend = {
-//         display: true,
-//         position: "bottom",
-//         labels: {
-//           fontColor: "#323130",
-//           fontSize: 14
-//         }
-//     };
-      
-//     const options = {
-//         title: {
-//           display: true,
-//           text: "Chart Title"
-//         },
-//         scales: {
-//           yAxes: [
-//             {
-//               ticks: {
-//                 suggestedMin: 0,
-//                 suggestedMax: 100
-//               }
-//             }
-//           ]
-//         }
-//     };
-
-
-//     return <Line data={data} legend={legend} options={options}/>
-// }
-
-export default LineChart
+export default LineChart;

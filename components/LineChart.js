e267e91a2eb9f89c2c142c389e3407ff72f3ed2a
Ameera Binte Atique <atiquea@uoguelph.ca>
2021-03-21 19:41:39 -0400

    please see lines 30-50

--- components/LineChart.js
+++ components/LineChart.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View } from "react-native";
 // import ReactDOM from 'react-dom';
 // import * as V from 'victory';
+
 import {
   VictoryLine,
   VictoryChart,
@@ -26,6 +27,32 @@ function LineChart(props) {
             { x: 4, y: 4 },
             { x: 5, y: 7 },
           ]}
+          width={Dimensions.get("window").width}
+          height={220}
+          yAxisLabel="step counts"
+          xAxisLabel="date"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+              backgroundColor: "#e26a00",
+              backgroundGradientFrom: "#fb8c00",
+              backgroundGradientTo: "#ffa726",
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
         />
       </VictoryChart>
     </View>

c0acb572119b72daa1adf432fd492273a22b9759
Ameera Binte Atique <atiquea@uoguelph.ca>
2021-03-22 19:36:36 -0400

    removed lines 24-55

--- components/LineChart.js
+++ components/LineChart.js
@@ -21,32 +21,7 @@ function LineChart(props) {
             parent: { border: "1px solid #ccc" },
           }}
           data={data}
-    //       width={Dimensions.get("window").width}
-    //       height={220}
-    //       yAxisLabel="step counts"
-    //       xAxisLabel="date"
-    //       yAxisInterval={1} // optional, defaults to 1
-    //       chartConfig={{
-    //           backgroundColor: "#e26a00",
-    //           backgroundGradientFrom: "#fb8c00",
-    //           backgroundGradientTo: "#ffa726",
-    //           decimalPlaces: 2, // optional, defaults to 2dp
-    //           color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    //           labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    //           style: {
-    //             borderRadius: 16
-    //           },
-    //       propsForDots: {
-    //         r: "6",
-    //         strokeWidth: "2",
-    //         stroke: "#ffa726"
-    //       }
-    //     }}
-    // bezier
-    // style={{
-    //   marginVertical: 8,
-    //   borderRadius: 16
-    // }}
+    
         />
       </VictoryChart>
     </View>

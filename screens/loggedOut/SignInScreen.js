238df40ed1e3671c9a6dd1d93795f3343524d0da
Srinath Natarajan <sriyegna@gmail.com>
2021-03-23 15:32:02 -0400

    Added forget password functionality

--- screens/loggedOut/SignInScreen.js
+++ screens/loggedOut/SignInScreen.js
@@ -16,6 +16,8 @@ import Feather from "react-native-vector-icons/Feather";
 import { useTheme } from "@react-navigation/native";
 import { AuthContext } from "../../contexts/context";
 import { validateEmail, validatePassword } from "../../utils/validator";
+import { Button, Paragraph, Dialog, Portal } from "react-native-paper";
+import Server from "../../utils/Server";
 
 const SignInScreen = ({ navigation }) => {
   const { colors } = useTheme();
@@ -86,97 +88,267 @@ const SignInScreen = ({ navigation }) => {
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
-      <View style={styles.header}>
-        <Text style={styles.text_header}>Welcome!</Text>
-      </View>
-      <Animatable.View
-        animation="fadeInUpBig"
-        style={[styles.footer, { backgroundColor: colors.background }]}
-      >
-        <Text style={[styles.text_footer, { color: colors.text }]}>Email</Text>
-        <View style={styles.action}>
-          <FontAwesome name="envelope-o" color={colors.text} size={20} />
-          <TextInput
-            placeholder="Email address"
-            style={[styles.textInput, { color: colors.text }]}
-            autoCapitalize="none"
-            onChangeText={(val) => handleEmailChange(val)}
-          />
-          {data.check_emailInputChange ? (
-            <Animatable.View animation="bounceIn">
-              <Feather name="check-circle" color="green" size={20} />
-            </Animatable.View>
-          ) : null}
-        </View>
-        {data.isValidEmail ? null : (
-          <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={styles.errorMsg}>Must be a valid email address.</Text>
-          </Animatable.View>
-        )}
-        <Text
-          style={[styles.text_footer, { color: colors.text, marginTop: 35 }]}
-        >
-          Password
-        </Text>
-        <View style={styles.action}>
-          <Feather name="lock" color={colors.text} size={20} />
-          <TextInput
-            placeholder="Password"
-            secureTextEntry={data.secureTextEntry}
-            style={[styles.textInput, { color: colors.text }]}
-            autoCapitalize="none"
-            onChangeText={(val) => handlePasswordChange(val)}
-          />
-          <TouchableOpacity onPress={() => updateSecureTextEntry()}>
-            {data.secureTextEntry ? (
-              <Feather name="eye-off" color="grey" size={20} />
-            ) : (
-              <Feather name="eye" color="grey" size={20} />
-            )}
-          </TouchableOpacity>
-        </View>
-        {data.isValidPassword ? null : (
-          <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={styles.errorMsg}>
-              Password must be 8 characters long.
-            </Text>
-          </Animatable.View>
-        )}
+  const [
+    forgotPasswordDialogVisible,
+    setForgotPasswordDialogVisible,
+  ] = useState(false);
+  const [resetEmailAddress, setResetEmailAddress] = useState("");
+  const [resetPassword, setResetPassword] = useState("");
+  const [resetAuthorized, setResetAuthorized] = useState(false);
+  const [resetSecureEntry, setResetSecureEntry] = useState(true);
 
-        <TouchableOpacity>
-          <Text style={{ color: "#8bc34a", marginTop: 15 }}>
-            Forgot password?
-          </Text>
-        </TouchableOpacity>
-        <View style={styles.button}>
-          <TouchableOpacity
-            style={styles.signIn}
-            onPress={() => {
-              handleLogin();
-            }}
-          >
-            <LinearGradient
-              colors={["#08d4c4", "#01ab9d"]}
-              style={styles.signIn}
+  const updateResetSecureEntry = () => {
+    setResetSecureEntry((prevState) => !prevState);
+  };
+
+  const handleResetEmailAddress = (val) => {
+    if (validateEmail(val)) {
+      setResetEmailAddress(val);
+    }
+  };
+
+  const handleResetPassword = (val) => {
+    if (validatePassword(val)) {
+      setResetPassword(val);
+    }
+  };
+
+  const submitResetRequest = async () => {
+    if (validateEmail(resetEmailAddress)) {
+      await Server.post("/api/user/reset", {
+        email: resetEmailAddress,
+      })
+        .then((res) => {
+          if (res.data.flag) {
+            setResetAuthorized(true);
+          } else {
+            Alert.alert(
+              "Success",
+              "Please check your email for a reset link.",
+              [
+                {
+                  text: "Cancel",
+                  style: "cancel",
+                },
+              ]
+            );
+            setResetAuthorized(false);
+            setForgotPasswordDialogVisible(false);
+          }
+        })
+        .catch((err) => {
+          Alert.alert("Error", JSON.stringify(err.response.data.message), [
+            { text: "Cancel", style: "cancel" },
+          ]);
+          setResetAuthorized(false);
+          setForgotPasswordDialogVisible(false);
+        });
+    } else {
+      Alert.alert("Error", "Email is not valid", [
+        { text: "Cancel", style: "cancel" },
+      ]);
+    }
+  };
+
+  const submitResetPasswordRequest = async () => {
+    if (validateEmail(resetEmailAddress) && validatePassword(resetPassword)) {
+      await Server.post("/api/user/resetpassword", {
+        email: resetEmailAddress,
+        password: resetPassword,
+      })
+        .then(() => {
+          Alert.alert("Success", "Please login with your new password", [
+            {
+              text: "Cancel",
+              style: "cancel",
+            },
+          ]);
+          setResetEmailAddress("");
+          setResetPassword("");
+          setResetAuthorized(false);
+          setForgotPasswordDialogVisible(false);
+        })
+        .catch((err) => {
+          Alert.alert("Error", JSON.stringify(err.response.data.message), [
+            { text: "Cancel", style: "cancel" },
+          ]);
+          setResetAuthorized(false);
+          setForgotPasswordDialogVisible(false);
+        });
+    } else {
+      Alert.alert("Error", "Email or password is not valid", [
+        { text: "Cancel", style: "cancel" },
+      ]);
+    }
+  };
+
+  return (
+    <>
+      <Portal>
+        <Dialog
+          visible={forgotPasswordDialogVisible}
+          onDismiss={() => setForgotPasswordDialogVisible(false)}
+        >
+          <Dialog.Title>Forgot Password</Dialog.Title>
+          <Dialog.Content>
+            {resetAuthorized ? (
+              <Paragraph>Enter the new password to set.</Paragraph>
+            ) : (
+              <Paragraph>Enter the email address of the account.</Paragraph>
+            )}
+            <View style={styles.action}>
+              <FontAwesome name="envelope-o" color={colors.text} size={20} />
+              <TextInput
+                placeholder="Email address"
+                style={[styles.textInput, { color: colors.text }]}
+                autoCapitalize="none"
+                onChangeText={(val) => handleResetEmailAddress(val)}
+              />
+            </View>
+            {resetAuthorized && (
+              <>
+                <Text
+                  style={[
+                    styles.text_footer,
+                    { color: colors.text, marginTop: 35 },
+                  ]}
+                >
+                  Password
+                </Text>
+                <View style={styles.action}>
+                  <Feather name="lock" color={colors.text} size={20} />
+                  <TextInput
+                    placeholder="Password"
+                    secureTextEntry={resetSecureEntry}
+                    style={[styles.textInput, { color: colors.text }]}
+                    autoCapitalize="none"
+                    onChangeText={(val) => handleResetPassword(val)}
+                  />
+                  <TouchableOpacity onPress={() => updateResetSecureEntry()}>
+                    {data.secureTextEntry ? (
+                      <Feather name="eye-off" color="grey" size={20} />
+                    ) : (
+                      <Feather name="eye" color="grey" size={20} />
+                    )}
+                  </TouchableOpacity>
+                </View>
+              </>
+            )}
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button
+              onPress={() =>
+                resetAuthorized
+                  ? submitResetPasswordRequest()
+                  : submitResetRequest()
+              }
             >
-              <Text style={[styles.textSign, { color: "#fff" }]}>Sign In</Text>
-            </LinearGradient>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigation.navigate("SignUpScreen")}
-            style={[
-              styles.signIn,
-              { borderColor: "#8bc34a", borderWidth: 1, marginTop: 15 },
-            ]}
-          >
-            <Text style={[styles.textSign, { color: "#8bc34a" }]}>Sign Up</Text>
-          </TouchableOpacity>
+              Submit
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      <View style={styles.container}>
+        <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
+        <View style={styles.header}>
+          <Text style={styles.text_header}>Welcome!</Text>
         </View>
-      </Animatable.View>
-    </View>
+        <Animatable.View
+          animation="fadeInUpBig"
+          style={[styles.footer, { backgroundColor: colors.background }]}
+        >
+          <Text style={[styles.text_footer, { color: colors.text }]}>
+            Email
+          </Text>
+          <View style={styles.action}>
+            <FontAwesome name="envelope-o" color={colors.text} size={20} />
+            <TextInput
+              placeholder="Email address"
+              style={[styles.textInput, { color: colors.text }]}
+              autoCapitalize="none"
+              onChangeText={(val) => handleEmailChange(val)}
+            />
+            {data.check_emailInputChange ? (
+              <Animatable.View animation="bounceIn">
+                <Feather name="check-circle" color="green" size={20} />
+              </Animatable.View>
+            ) : null}
+          </View>
+          {data.isValidEmail ? null : (
+            <Animatable.View animation="fadeInLeft" duration={500}>
+              <Text style={styles.errorMsg}>
+                Must be a valid email address.
+              </Text>
+            </Animatable.View>
+          )}
+          <Text
+            style={[styles.text_footer, { color: colors.text, marginTop: 35 }]}
+          >
+            Password
+          </Text>
+          <View style={styles.action}>
+            <Feather name="lock" color={colors.text} size={20} />
+            <TextInput
+              placeholder="Password"
+              secureTextEntry={data.secureTextEntry}
+              style={[styles.textInput, { color: colors.text }]}
+              autoCapitalize="none"
+              onChangeText={(val) => handlePasswordChange(val)}
+            />
+            <TouchableOpacity onPress={() => updateSecureTextEntry()}>
+              {data.secureTextEntry ? (
+                <Feather name="eye-off" color="grey" size={20} />
+              ) : (
+                <Feather name="eye" color="grey" size={20} />
+              )}
+            </TouchableOpacity>
+          </View>
+          {data.isValidPassword ? null : (
+            <Animatable.View animation="fadeInLeft" duration={500}>
+              <Text style={styles.errorMsg}>
+                Password must be 8 characters long.
+              </Text>
+            </Animatable.View>
+          )}
+
+          <TouchableOpacity
+            onPress={() => setForgotPasswordDialogVisible(true)}
+          >
+            <Text style={{ color: "#8bc34a", marginTop: 15 }}>
+              Forgot password?
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.button}>
+            <TouchableOpacity
+              style={styles.signIn}
+              onPress={() => {
+                handleLogin();
+              }}
+            >
+              <LinearGradient
+                colors={["#08d4c4", "#01ab9d"]}
+                style={styles.signIn}
+              >
+                <Text style={[styles.textSign, { color: "#fff" }]}>
+                  Sign In
+                </Text>
+              </LinearGradient>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SignUpScreen")}
+              style={[
+                styles.signIn,
+                { borderColor: "#8bc34a", borderWidth: 1, marginTop: 15 },
+              ]}
+            >
+              <Text style={[styles.textSign, { color: "#8bc34a" }]}>
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Animatable.View>
+      </View>
+    </>
   );
 };
 

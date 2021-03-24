ebe649e55e8430d35b44393aca5a1f3063a99a31
Srinath Natarajan <sriyegna@gmail.com>
2021-03-24 11:07:27 -0400

    Added daily notification

--- screens/loggedIn/drawer/DrawerScreen2.js
+++ screens/loggedIn/drawer/DrawerScreen2.js
@@ -1,18 +1,65 @@
-import React from "react";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, StatusBar, Platform } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { useTheme } from "@react-navigation/native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Button } from "react-native-paper";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export const DrawerScreen2 = ({ navigation }) => {
   const { colors } = useTheme();
-
   const theme = useTheme();
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
+    setNotifications(currentDate);
+  };
+
+  const setNotifications = (date) => {
+    Notifications.cancelAllScheduledNotificationsAsync();
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Pedometer",
+        body: "Enter your weight for today!",
+      },
+      trigger: {
+        repeats: true,
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+      },
+    });
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
-      <Text style={{ color: colors.text }}>Drawer2</Text>
+      <Text style={{ color: colors.text }}>
+        Set the time you want to be reminded to enter your weight daily:
+      </Text>
+      <Button onPress={() => setShow(true)}>Set time</Button>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="time"
+          is24Hour={false}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
@@ -32,7 +79,7 @@ const DrawerStackScreen2 = ({ navigation }) => (
       name="Drawer2"
       component={DrawerScreen2}
       options={{
-        title: "Drawer2",
+        title: "Notifications",
         headerLeft: () => (
           <Icon.Button
             name="menu"

214b76cf6380599caead92d85975608f4f595896
Siri Chandana Kathroju <skathroj@uoguelph.ca>
2021-04-05 22:18:31 +0000

    Resolve "As a user, I want to toggle between graphs, so that I can compare multiple habits at once."

--- screens/loggedIn/tabs/TabScreen3.js
+++ screens/loggedIn/tabs/TabScreen3.js
@@ -1,39 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, StatusBar } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { useTheme } from "@react-navigation/native";
 import * as SQLite from "expo-sqlite";
-import { DB_FILE, DB_WEIGHT_TABLE, DB_HEIGHT_TABLE } from "../../../constants";
+import { DB_FILE, DB_WEIGHT_TABLE, DB_HEIGHT_TABLE, DB_PEDOMETER_TABLE } from "../../../constants";
 import LineChart from "../../../components/LineChart"
 import { Line } from "react-chartjs-2";
+import { TextInput, DataTable } from "react-native-paper";
 
-import { epochToDate, dateToEpoch, getPreviousDate } from "../../../utils/datetime";
+import { epochToDate, dateToEpoch, getPreviousDate, getCurrentDateInEpoch, getCurrentDate } from "../../../utils/datetime";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
 
 const db = SQLite.openDatabase(DB_FILE);
 
 export const TabScreen3 = ({ navigation }) => {
-  // useEffect(() => {
-  //   db.transaction((tx) => {
-  //     tx.executeSql(`select * from ${DB_PEDOMETER_TABLE}`, [], (_, { rows }) =>
-  //       console.log(JSON.stringify(rows))
-  //     );
-  //   });
-  // }, []);
+  const { colors } = useTheme();
+  const theme = useTheme();
 
   const [data, setData] = useState([]);
+  const [weight, setWeight] = useState([]);
+  const [steps, setSteps] = useState([]);
+  const [visibleView, setVisibleView] = useState('BMI') // 'BMI', 'WEIGHT', 'STEPS'
+
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
-      const id = dateToEpoch(getPreviousDate(6));
-      db.transaction((tx) => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    const id = getCurrentDateInEpoch();
+    db.transaction((tx) => {
+      tx.executeSql(`SELECT * FROM ${DB_WEIGHT_TABLE}`, [], (_, { rows }) => {
+        setTableData(rows["_array"]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    const id = dateToEpoch(getPreviousDate(6));
+    db.transaction((tx) => {
       tx.executeSql(
-        `select * from ${DB_WEIGHT_TABLE} WHERE id >= ${id}`, 
-        [], 
+        `select * from ${DB_WEIGHT_TABLE} WHERE id >= ${id}`,
+        [],
         (_, { rows }) => {
           setData(
             rows["_array"].map((x) => ({
               // bmi formula is kg/m^2 
-              y: (Math.floor((Number(x.weight * 0.453592)/ (1.75*1.75)*100)))/ 100,
+              y: Number((Math.floor((Number(x.weight * 0.453592) / (1.75 * 1.75) * 100))) / 100),
               x: epochToDate(x["id"]),
               // labels: x.steps,
             }))
@@ -43,14 +58,110 @@ export const TabScreen3 = ({ navigation }) => {
     });
   }, []);
 
+  useEffect(() => {
+    const id = dateToEpoch(getPreviousDate(6));
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from ${DB_WEIGHT_TABLE} WHERE id >= ${id}`,
+        [],
+        (_, { rows }) => {
+          setWeight(
+            rows["_array"].map((x) => ({
+
+              y: Number(x.weight),
+              x: epochToDate(x["id"]),
+
+            }))
+          );
+        }
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    const id = dateToEpoch(getPreviousDate(6));
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from ${DB_PEDOMETER_TABLE} WHERE id >= ${id}`,
+        [],
+        (_, { rows }) => {
+          setSteps(
+            rows["_array"].map((x) => ({
+              y: Number(x.steps),
+              x: epochToDate(x["id"]),
+              // labels: x.steps,
+            }))
+          );
+        }
+      );
+    });
+  }, []);
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Body Mass Index for Past Week</Text>
-      <LineChart data={data}/>
-    </View>
+    <ScrollView>
+      <View>
+        <StatusBar barStyle={theme.dark ? "dark-content" : "light-content"} />
+      </View>
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button title="BMI" onPress={() => setVisibleView('BMI')}/>
+          <Button title="Weight" onPress={() => setVisibleView('WEIGHT')} />
+          <Button title="Steps" onPress={() => setVisibleView('STEPS')} />
+        </View>
+        
+        {visibleView === 'BMI' &&
+          <>
+            <Text>Displaying BMI over the past week</Text>
+            <LineChart data={data} />
+          </>
+        }
+        {visibleView === 'WEIGHT' &&
+          <>
+            <Text>Displaying weight over the past week</Text>
+            <LineChart data={weight} />
+            
+          </>
+        }
+        {visibleView === 'STEPS' &&
+          <>
+            <Text>Displaying steps over the past week</Text>
+            <LineChart data={steps} />
+          </>
+        }
+      </View>
+      <View>
+        <View>
+          <View>
+            <DataTable>
+              <DataTable.Header>
+                <DataTable.Title>Date</DataTable.Title>
+                <DataTable.Title>Weight</DataTable.Title>
+                <DataTable.Title>BMI</DataTable.Title>
+                <DataTable.Title>Steps</DataTable.Title>
+              </DataTable.Header>
+              {tableData.map((x) => {
+                return (
+                  <DataTable.Row key={x.id}>
+                    <DataTable.Cell>
+                      {epochToDate(x.id).toDateString()}
+                    </DataTable.Cell>
+                    <DataTable.Cell>{x.weight}</DataTable.Cell>
+                    <DataTable.Cell>{x.bmi}</DataTable.Cell>
+                    <DataTable.Cell>{x.steps}</DataTable.Cell>
+                  </DataTable.Row>
+                );
+              })}
+            </DataTable>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+    
   );
 };
-// testing 123
+
 const TabStack3 = createStackNavigator();
 
 const TabStackScreen3 = ({ navigation }) => {
@@ -66,10 +177,10 @@ const TabStackScreen3 = ({ navigation }) => {
       }}
     >
       <TabStack3.Screen
-        name="BMI"
+        name="Trends"
         component={TabScreen3}
         options={{
-          title: "BMI",
+          title: "Trends",
           headerLeft: () => (
             <Icon.Button
               name="menu"
@@ -92,4 +203,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  buttons: {
+
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 10,
+    paddingBottom: 20
+  }
 });
